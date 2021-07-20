85055ccbed11dcad9c2813b999cdfe48407965cd
Gabrieldevelop <82325681+Gabrieldevelop@users.noreply.github.com>
2021-07-20 19:07:40 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,3 +10,32 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times')
     navbar.classList.remove('nav-toggle');
 }
+
+let pop_up = document.querySelectorAll('.gallery .row img');
+
+pop_up.forEach(image  =>{
+   image.onclick = () =>{
+    document.querySelector('.pop-up').style.display = 'block';
+    document.querySelector('.pop-up img').src = image.getAttribute('src');
+    document.querySelector('.gallery .download-btn').style.display = 'block';
+   }
+});
+
+let close_img = document.querySelector('.pop-up span');
+
+close_img.addEventListener('click', function(){
+    document.querySelector('.pop-up').style.display = 'none';
+});
+
+let alerta = document.querySelector('.gallery .download-btn');
+
+alerta.addEventListener('click', function(){
+    alert("Hola, gracias por visitar mi sitio web en una próxima actualización podrás descargar las imágenes \nAtt: Raymer Gabriel 😀");
+});
+
+
+
+
+
+
+
